d3b3ad38fa65cb67a3f4cad903c1b34ac99938ea
Rajendra Kokare <kokare.raju@gmail.com>
2026-02-10 22:31:34 +0530

    Day 3: Loops practice

---
+++ day3_loops.py
@@ -0,0 +1,9 @@
+# Day 3 - Loops in Python
+print("Numbers from 1 to5")
+for i in range(1,6):
+    print(i)
+print("Even Numbers from 1 to 10")
+for i in range(1,11):
+    if i %2==0:
+        print(i)
+        
